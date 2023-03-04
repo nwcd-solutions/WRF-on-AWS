70421d1e148e0f76614da1fb3aadb5ce148bd7f1
nwcd-solutions <62685513+nwcd-solutions@users.noreply.github.com>
2023-03-04 09:16:04 +0000

    Create slurm_run_wrf.sh

---
+++ scripts/fsx/slurm_run_wrf.sh
@@ -0,0 +1,11 @@
+#!/bin/bash 
+#SBATCH --error=job.err
+#SBATCH --output=job.out
+#SBATCH --time=24:00:00
+#SBATCH --job-name=wrf
+#SBATCH --nodes=2
+#SBATCH --ntasks-per-node=64
+#SBATCH --cpus-per-task=1
+
+cd /shared/FORECAST/domains/test/run
+mpirun  ./wrf.exe
