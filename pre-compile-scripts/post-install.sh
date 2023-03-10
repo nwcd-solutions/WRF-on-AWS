4322b926e6eddd13fa2e29dc8f25598e379b14d5
nwcd-solutions <62685513+nwcd-solutions@users.noreply.github.com>
2023-03-10 03:13:55 +0000

    Update post-install.sh

--- pre-compile-scripts/post-install.sh
+++ pre-compile-scripts/post-install.sh
@@ -1,3 +1,5 @@
-#!bin/bash -xe
+#!/bin/bash -xe
+#Load Parallelcluster environment variables
+. /etc/parallelcluster/cfnconfig
 cd /fsx
 aws s3 cp https://aws-hpc-builder.s3.amazonaws.com/project/wrf/aws_pcluster_3.4_al2_wrf_aarch64.tar.xz .
