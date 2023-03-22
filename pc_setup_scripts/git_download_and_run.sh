49ef24bd38c1a0c54f8e34be4465ed66e6ec9eb4
nwcd-solutions <62685513+nwcd-solutions@users.noreply.github.com>
2023-03-22 11:21:55 +0000

    Update git_download_and_run.sh

--- pc_setup_scripts/git_download_and_run.sh
+++ pc_setup_scripts/git_download_and_run.sh
@@ -4,7 +4,8 @@
 
 github_repo=$(echo ${cfn_postinstall_args}| cut -d ',' -f 1 )
 setup_command=$(echo ${cfn_postinstall_args}| cut -d ',' -f 2 )
-shared_folder=$(echo $cfn_ebs_shared_dirs | cut -d ',' -f 1 )
+#shared_folder=$(echo $cfn_ebs_shared_dirs | cut -d ',' -f 1 )
+shared_folder=/fsx
 
 echo "ARUMENTS $cfn_postinstall_args"
 echo "REPO: ${github_repo}"
