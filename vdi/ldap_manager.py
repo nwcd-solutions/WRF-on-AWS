e3d5172a3042f0c18f68aa60d1c78c830a1ddc65
nwcd-solutions <62685513+nwcd-solutions@users.noreply.github.com>
2024-01-13 01:24:35 +0000

    Update ldap_manager.py

--- vdi/ldap_manager.py
+++ vdi/ldap_manager.py
@@ -16,18 +16,97 @@ import subprocess
 import datetime
 
 def run_command(cmd):
+    try:
+        command = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        stdout, stderr = command.communicate()
+        return stdout
+    except subprocess.CalledProcessError as e:
         exit(1)
 
 
 def find_ids():
+    used_uid = []
+    used_gid = []
+    res = con.search_s(ldap_base,
+                       ldap.SCOPE_SUBTREE,
+                       'objectClass=posixAccount', ['uidNumber', 'gidNumber']
+                       )
+    # Any users/group created will start with uid/gid => 5000
+    uid = 5000
+    gid = 5000
+    for a in res:
+        uid_temp = int(a[1].get('uidNumber')[0])
+        used_uid.append(uid_temp)
+        if uid_temp > uid:
+            uid = uid_temp
+
+    for a in res:
+        gid_temp = int(a[1].get('gidNumber')[0])
+        used_gid.append(gid_temp)
+        if gid_temp > gid:
+            gid = gid_temp
+
+    return {'next_uid': int(uid) + 1,
+            'used_uid': used_uid,
+            'next_gid': int(gid) + 1,
             'used_gid': used_gid}
 
 
 def create_home(username):
+    try:
+        key = rsa.generate_private_key(backend=crypto_default_backend(), public_exponent=65537, key_size=2048)
+        private_key = key.private_bytes(
+            crypto_serialization.Encoding.PEM,
+            crypto_serialization.PrivateFormat.TraditionalOpenSSL,
+            crypto_serialization.NoEncryption())
+        public_key = key.public_key().public_bytes(
+            crypto_serialization.Encoding.OpenSSH,
+            crypto_serialization.PublicFormat.OpenSSH
+        )
+        private_key_str = private_key.decode('utf-8')
+        public_key_str = public_key.decode('utf-8')
+        # Create user directory structure and permissions
+        user_path = user_home + '/' + username + '/.ssh'
+        run_command('mkdir -p ' + user_path)
+        shutil.copy('/etc/skel/.bashrc', user_path[:-4])
+        shutil.copy('/etc/skel/.bash_profile', user_path[:-4])
+        shutil.copy('/etc/skel/.bash_logout', user_path[:-4])
+        shutil.chown(user_home + '/' + username + '/.bashrc', user=username, group=username)
+        shutil.chown(user_home + '/' + username + '/.bash_profile', user=username, group=username)
+        shutil.chown(user_home + '/' + username + '/.bash_logout', user=username, group=username)
+        print(private_key_str, file=open(user_path + '/id_rsa', 'w'))
+        print(public_key_str, file=open(user_path + '/id_rsa.pub', 'w'))
+        print(public_key_str, file=open(user_path + '/authorized_keys', 'w'))
+        shutil.chown(user_home + '/' + username, user=username, group=username)
+        shutil.chown(user_home + '/' + username + '/.ssh', user=username, group=username)
+        shutil.chown(user_home + '/' + username + '/.ssh/authorized_keys', user=username, group=username)
+        shutil.chown(user_home + '/' + username + '/.ssh/id_rsa', user=username, group=username)
+        shutil.chown(user_home + '/' + username + '/.ssh/id_rsa.pub', user=username, group=username)
+        os.chmod(user_home + '/' + username, 0o750)
+        os.chmod(user_home + '/' + username + '/.ssh', 0o700)
+        os.chmod(user_home + '/' + username + '/.ssh/id_rsa', 0o600)
+        os.chmod(user_home + '/' + username + '/.ssh/authorized_keys', 0o600)
+        return True
+    except Exception as e:
+        exc_type, exc_obj, exc_tb = sys.exc_info()
+        fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+        print(exc_type, fname, exc_tb.tb_lineno)
         return e
 
 
 def create_group(username, gid_number):
+    dn_group = "cn=" + username + ",ou=group," + ldap_base
+    attrs = [
+        ('objectClass', ['top'.encode('utf-8'),
+                         'posixGroup'.encode('utf-8')]),
+        ('gidNumber', [str(gid_number).encode('utf-8')]),
+        ('cn', [str(username).encode('utf-8')])
+    ]
+
+    try:
+        con.add_s(dn_group, attrs)
+        return True
+    except Exception as e:
         return e
 
 
